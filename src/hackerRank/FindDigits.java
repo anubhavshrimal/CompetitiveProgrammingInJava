8c86affe841817ec864f6f72a1a6db68bbc1615d
Anubhav Shrimal <anubhavshrimal@gmail.com>
2016-04-13 23:12:02 +0530

    added new problem in hackerRank named FindDigits

--- src/hackerRank/FindDigits.java
+++ src/hackerRank/FindDigits.java
@@ -1,7 +1,22 @@
 package hackerRank;
+/*Given an integer, NN, traverse its digits (dd1,dd2,...,ddn) and determine how many digits evenly divide NN (i.e.: count the number of times NN divided by each digit ddi has a remainder of 00). Print the number of evenly divisible digits.
+
+Note: Each digit is considered to be unique, so each occurrence of the same evenly divisible digit should be counted (i.e.: for N=111N=111, the answer is 33).
+
+Input Format
+
+The first line is an integer, TT, indicating the number of test cases. 
+The TT subsequent lines each contain an integer, NN.
+
+Constraints 
+1≤T≤151≤T≤15 
+0<N<1090<N<109
+Output Format
+
+For every test case, count and print (on a new line) the number of digits in NN that are able to evenly divide NN.*/
+
 import java.io.*;
 import java.util.*;
- 
  
 public class FindDigits {
 
